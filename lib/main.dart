633990a1ad31febc1c6ab6e66a6b378b7e9a49f4
jon-mun <jonathan.edmund@gmail.com>
2023-03-04 16:11:15 +0700

    add login auth functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'views/home_page.dart';
-
+import 'package:veggytably_driver/views/login_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }

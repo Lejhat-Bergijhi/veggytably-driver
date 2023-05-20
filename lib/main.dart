de45570000652b1631ecbae391b21ed2a74a9d8a
vincalaudia <claudiajovinca@gmail.com>
2023-05-21 00:06:37 +0700

    Added 4 Digit Food Code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:veggytably_driver/controllers/profile_controller.dart';
+import 'package:veggytably_driver/views/home_page.dart';
 //import 'views/home_page.dart';
 import 'controllers/auth_controller.dart';
 import 'views/profile_page.dart';
@@ -18,7 +19,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LoginPage(),
+        home: HomePage(),
         initialBinding: BindingsBuilder(() {
           Get.put(AuthController());
           // Get.put(ProfileController());

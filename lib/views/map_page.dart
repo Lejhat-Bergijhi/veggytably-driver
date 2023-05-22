02fec4e4fb7fca5010704fa16c828bf6f7e4af96
jon-mun <jonathan.edmund@gmail.com>
2023-05-22 12:11:10 +0700

    fix map dependency conflict

--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -12,7 +13,7 @@ class MapPage extends StatelessWidget {
       ),
       body: FlutterMap(
         options: MapOptions(
-          // center: LatLng(-7.758668, 110.3806492),
+          center: LatLng(-7.758668, 110.3806492),
           zoom: 13.0,
         ),
         children: [
@@ -25,29 +26,29 @@ class MapPage extends StatelessWidget {
           MarkerLayer(
             markers: [
               // start marker
-              // Marker(
-              //   point: LatLng(-7.758668, 110.3806492),
-              //   width: 80,
-              //   height: 80,
-              //   builder: (context) => IconButton(
-              //     onPressed: () {},
-              //     icon: const Icon(Icons.location_on),
-              //     color: Colors.green,
-              //     iconSize: 45,
-              //   ),
-              // ),
-              // // end marker
-              // Marker(
-              //   point: LatLng(-7.7674489, 110.3761053),
-              //   width: 80,
-              //   height: 80,
-              //   builder: (context) => IconButton(
-              //     onPressed: () {},
-              //     icon: const Icon(Icons.location_on),
-              //     color: Colors.red,
-              //     iconSize: 45,
-              //   ),
-              // ),
+              Marker(
+                point: LatLng(-7.758668, 110.3806492),
+                width: 80,
+                height: 80,
+                builder: (context) => IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.location_on),
+                  color: Colors.green,
+                  iconSize: 45,
+                ),
+              ),
+              // end marker
+              Marker(
+                point: LatLng(-7.7674489, 110.3761053),
+                width: 80,
+                height: 80,
+                builder: (context) => IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.location_on),
+                  color: Colors.red,
+                  iconSize: 45,
+                ),
+              ),
             ],
           )
         ],
